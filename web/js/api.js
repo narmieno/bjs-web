a82c5a6a24708d6f74807b23fcdb6843b70eb676
Ayk Borstelmann <borstelmann.ayk@gmail.com>
2020-01-13 18:07:07 +0100

    Update API url

--- web/js/api.js
+++ web/js/api.js
@@ -1,4 +1,4 @@
-const api_url = "http://localhost:8080/api/v1";
+const api_url = "http://raspberry-balena.gtdbqv7ic1ie9w3s.myfritz.net:8080/api/v1";
 
 async function getClasses() {
     return fetch(`${api_url}/classes`, {
